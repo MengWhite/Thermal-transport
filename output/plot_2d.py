9c5535d8a9ac07b1a28e1ffac2d102e06c4a0c86
MengWhite <meng060915@163.com>
2024-09-11 16:09:08 +0800

    modified:   output/plot_2d.py

--- output/plot_2d.py
+++ output/plot_2d.py
@@ -131,3 +131,19 @@ def plot_scatter(x_plot, y_plot, x_scatter, y_scatter, xlabel, ylabel, ymin=None
 
     plt.close('all')
     return cav
+
+def background(xlabel, ylabel):
+    from matplotlib import pyplot as plt
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+    fig = plt.figure()
+    fig.patch.set_facecolor('w')
+    cav = FigureCanvas(fig)
+    ax = fig.subplots()
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    plt.subplots_adjust(bottom=0.3)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    ax.grid(ls = "--", lw = 0.5, color = "#4E616C") # 背景线
+    ax.patch.set_alpha(0)
+    return cav
